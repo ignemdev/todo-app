00f36c6702f4dc08d4e0bb408215879bccfcf085
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2023-04-08 00:07:49 -0400

    formatting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,25 @@
 //add todos
 $("input[type='text']").keypress(function (e) {
-    if (e.which === 13) {
-        let newTodo = $(this).val();
-        $(this).val("");
-        $('ul').append(`<li><span><i class="fa fa-trash" aria-hidden="true"></i></span> ${newTodo}</li>`);
-    };
+  if (e.which === 13) {
+    let newTodo = $(this).val();
+    $(this).val("");
+    $("ul").append(
+      `<li><span><i class="fa fa-trash" aria-hidden="true"></i></span> ${newTodo}</li>`
+    );
+  }
 });
 
 //check todos
-$("ul").on('click', 'li', function () {
-    $(this).toggleClass("checked");
+$("ul").on("click", "li", function () {
+  $(this).toggleClass("checked");
 });
 
 //delete todos
-$("ul").on('click', 'span', function (e) {
-    $(this).parent().fadeOut(1000, function () {
-        $(this).remove();
+$("ul").on("click", "span", function (e) {
+  $(this)
+    .parent()
+    .fadeOut(1000, function () {
+      $(this).remove();
     });
-    e.stopPropagation();
+  e.stopPropagation();
 });
